5d5f0867bde4c115cf6b5c57527c05a165317b15
devonfaraday <christianmcmullin@gmail.com>
2017-01-09 11:44:59 -0700

    Rating control initializers started

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -12,6 +12,12 @@ class RatingControl: UIStackView {
 
     //MARK: Initialization
     
-    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
 
 }
